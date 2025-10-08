1f5c6d6a377f2d8d1aa81f4769ce15c325c52f43
Tommy <tomzying@mail.ncut.edu.cn>
2025-10-08 22:46:39 +0800

    '更新样式'

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ function appendToDisplay(input) {
     display.value += input
 }
 
-function clearDisplay() {
+function allClear() {
     display.value = ''
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value)
+        const expression = display.value.replace(/%/g, "/100")
+        display.value = eval(expression)
     }
     catch(error) {
         display.value = "Invaild Input"
+    }
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1)
+}
+
+function toggleSign() {
+    if (display.value == '') return
+    try {eval(display.value)} catch(error) {return}
+    if (display.value.startsWith('(') && display.value.endsWith(')')) {
+        display.value = display.value.slice(1, -1);
+    }
+    if (display.value.startsWith('-')) {
+        display.value = display.value.slice(1)
+    } else {
+        display.value = '(-' + display.value + ')'
     }
 }
